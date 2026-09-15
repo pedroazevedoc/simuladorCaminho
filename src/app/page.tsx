e8ba9ec6e5fb33b15d68b5991ba3ddbbd9bebc43
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-09-15 20:56:03 -0300

    refactor(layout): componentizar formulário que estava na página principal e aprimorar a responsividade do formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { RespostaRota } from '@/types/city';
-import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxList, ComboboxTrigger } from '@/components/kibo-ui/combobox';
 import { Header } from '@/components/layouts/header';
-import { ShimmerButton } from '@/components/ui/shimmer-button';
-import { Navigation } from 'lucide-react';
+import { FormularioRota } from '@/components/layouts/formularioRota';
 import { Spinner } from '@/components/kibo-ui/spinner';
-import { Label } from '@/components/ui/label';
 import { LOCAIS } from '@/mocks/cityMocks';
 
 const CityMap3D = dynamic(() => import('@/components/city3d/cityMap3D'), {
@@ -51,78 +48,14 @@ export default function Home() {
         subtitle="Algoritmo do Menor Caminho (Teoria dos Grafos)"
       />
 
-      <div className="flex flex-wrap gap-4 my-6 bg-card p-4 rounded-lg border border-border shadow-lg">
-        {/* Origem */}
-        <div>
-          <Label htmlFor="origem" className="text-xs text-primary mb-1">Origem:</Label>
-          <Combobox
-            data={LOCAIS}
-            type="origem"
-            onValueChange={setOrigem}
-            value={origem}
-          >
-            <ComboboxTrigger />
-            <ComboboxContent>
-              <ComboboxInput />
-              <ComboboxEmpty />
-              <ComboboxList>
-                <ComboboxGroup>
-                  {LOCAIS.map((loc) => (
-                    <ComboboxItem key={loc.value} value={loc.value}>
-                      {loc.label}
-                    </ComboboxItem>
-                  ))}
-                </ComboboxGroup>
-              </ComboboxList>
-            </ComboboxContent>
-          </Combobox>
-        </div>
-
-        {/* Destino */}
-        <div>
-          <Label htmlFor="destino" className="text-xs text-primary mb-1">Destino:</Label>
-          <Combobox
-            data={LOCAIS}
-            type="destino"
-            onValueChange={setDestino}
-            value={destino}
-          >
-            <ComboboxTrigger />
-            <ComboboxContent>
-              <ComboboxInput />
-              <ComboboxEmpty />
-              <ComboboxList>
-                <ComboboxGroup>
-                  {LOCAIS.map((loc) => (
-                    <ComboboxItem key={loc.value} value={loc.value}>
-                      {loc.label}
-                    </ComboboxItem>
-                  ))}
-                </ComboboxGroup>
-              </ComboboxList>
-            </ComboboxContent>
-          </Combobox>
-        </div>
-
-        <div className="flex items-end">
-          <ShimmerButton
-            onClick={handleCalcularRota}
-            disabled={carregando}
-            shimmerColor="#f54900"
-            shimmerSize="0.12rem"
-            shimmerDuration="2s"
-            background="rgba(15, 23, 42, 0.9)"
-            className="relative flex items-center justify-center gap-3 px-6 py-2 rounded-xl font-bold text-white transition-all duration-300 shadow-lg shadow-orange-500/10 hover:shadow-orange-500/25 hover:scale-[1.02] active:scale-[0.98] border border-orange-500/30 hover:border-orange-500/60 disabled:opacity-50 disabled:cursor-not-allowed group cursor-pointer"
-          >
-            {/* Ícone com animação de pulso no hover */}
-            <Navigation className={`w-5 h-5 text-orange-400 transition-transform duration-300 group-hover:rotate-45 ${carregando ? 'animate-spin' : ''}`} />
-            
-            <span className="tracking-wide bg-linear-to-r from-orange-200 via-white to-orange-100 bg-clip-text text-transparent">
-              {carregando ? 'Calculando Rota...' : 'Calcular Menor Rota'}
-            </span>
-          </ShimmerButton>
-        </div>
-      </div>
+      <FormularioRota
+        origem={origem}
+        destino={destino}
+        carregando={carregando}
+        onOrigemChange={setOrigem}
+        onDestinoChange={setDestino}
+        onCalcular={handleCalcularRota}
+      />
 
       {resultado && (
         <div className="mb-4 text-emerald-500 font-semibold bg-emerald-950/60 border border-emerald-800/50 px-4 py-2 rounded-md">
